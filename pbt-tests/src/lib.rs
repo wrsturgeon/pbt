b53f29ccbd1ec6e3afd35ef44b81876dee9deda7
Will Sturgeon <willstrgn@gmail.com>
2026-03-29 02:02:12 -0700

    chore: update `pbt-tests` after update

--- pbt-tests/src/lib.rs
+++ pbt-tests/src/lib.rs
@@ -38,68 +38,56 @@ mod test {
                 Doggo::Subwoofer {
                     many_wow: 358,
                     such_amaze: 79,
-                    pals: vec![Doggo::Subwoofer {
-                        many_wow: 1,
-                        such_amaze: 40,
-                        pals: vec![],
-                    }],
+                    pals: vec![Doggo::Woofer,],
                 },
                 Doggo::Subwoofer {
-                    many_wow: 1,
-                    such_amaze: 175,
-                    pals: vec![Doggo::Subwoofer {
-                        many_wow: 0,
-                        such_amaze: 4542,
-                        pals: vec![],
-                    }],
-                },
-                Doggo::Subwoofer {
-                    many_wow: 2,
-                    such_amaze: 59,
+                    many_wow: 0,
+                    such_amaze: 4,
                     pals: vec![],
                 },
+                Doggo::Woofer,
                 Doggo::Subwoofer {
-                    many_wow: 1,
-                    such_amaze: 59,
+                    many_wow: 3,
+                    such_amaze: 13823,
                     pals: vec![Doggo::Subwoofer {
                         many_wow: 0,
-                        such_amaze: 9,
+                        such_amaze: 0,
                         pals: vec![Doggo::Subwoofer {
-                            many_wow: 0,
-                            such_amaze: 9,
+                            many_wow: 3,
+                            such_amaze: 15952,
                             pals: vec![],
-                        }],
-                    }],
+                        },],
+                    },],
                 },
                 Doggo::Subwoofer {
-                    many_wow: 0,
+                    many_wow: 2116,
                     such_amaze: 1,
-                    pals: vec![Doggo::Subwoofer {
-                        many_wow: 8,
-                        such_amaze: 1,
-                        pals: vec![Doggo::Woofer],
-                    }],
-                },
-                Doggo::Subwoofer {
-                    many_wow: 0,
-                    such_amaze: 88,
-                    pals: vec![Doggo::Subwoofer {
-                        many_wow: 1,
-                        such_amaze: 18446744073709551615,
-                        pals: vec![],
-                    }],
-                },
-                Doggo::Subwoofer {
-                    many_wow: 11,
-                    such_amaze: 15,
                     pals: vec![
                         Doggo::Woofer,
                         Doggo::Subwoofer {
-                            many_wow: 0,
+                            many_wow: 12083733,
                             such_amaze: 0,
                             pals: vec![],
                         },
                     ],
+                },
+                Doggo::Subwoofer {
+                    many_wow: 12,
+                    such_amaze: 5,
+                    pals: vec![Doggo::Subwoofer {
+                        many_wow: 18446744073709551615,
+                        such_amaze: 12,
+                        pals: vec![Doggo::Woofer, Doggo::Woofer,],
+                    },],
+                },
+                Doggo::Subwoofer {
+                    many_wow: 6,
+                    such_amaze: 0,
+                    pals: vec![Doggo::Subwoofer {
+                        many_wow: 10,
+                        such_amaze: 945336233,
+                        pals: vec![],
+                    },],
                 },
             ],
         );
