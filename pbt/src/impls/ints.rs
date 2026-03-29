dcf219e2cd76e699346efc3c3c2d720320d5a6a1
Will Sturgeon <willstrgn@gmail.com>
2026-03-29 03:11:30 -0700

    feat: feature-gated impl for `malachite::Natural`

--- pbt/src/impls/ints.rs
+++ pbt/src/impls/ints.rs
@@ -1,5 +1,104 @@
 //! Implementations for int-like types.
 
+#[cfg(feature = "malachite")]
+mod malachite {
+    #![allow(
+        clippy::allow_attributes,
+        clippy::wildcard_imports,
+        reason = "the purpose of this effectively transparent module is only feature-gating"
+    )]
+
+    //! Implementations for numeric types from the `malachite` crate.
+
+    use {
+        super::*,
+        ::malachite::{
+            Natural,
+            base::num::basic::traits::{One as _, Zero as _},
+            platform::Limb,
+        },
+    };
+
+    impl Construct for Natural {
+        #[inline]
+        fn arbitrary_fields_for_ctor(
+            _ctor_idx: NonZero<usize>,
+            _prng: &mut WyRand,
+            _size: Size,
+        ) -> TermsOfVariousTypes {
+            TermsOfVariousTypes::new()
+        }
+
+        #[inline]
+        fn register_all_immediate_dependencies(_visited: &BTreeSet<Type>) {}
+
+        #[inline]
+        fn type_former() -> TypeFormer<Self> {
+            TypeFormer::Literal(Literal {
+                corners: vec![
+                    Self::ZERO,
+                    Self::ONE,
+                    Self::from_owned_limbs_asc(vec![Limb::MAX]),
+                    Self::from_owned_limbs_asc(vec![0, 1]),
+                    Self::from_owned_limbs_asc(vec![0, 0, 1]), // very, very large
+                ],
+                generate: |prng| {
+                    // Copied with small (unfortunately incompatible)
+                    // modifications from `arbitrary_unsigned` above.
+
+                    // SAFETY: 4 != 0
+                    let mut one_in_n = unsafe { NonZero::new_unchecked(4_u64) };
+
+                    if (prng.rand() % one_in_n) == 0 {
+                        return Self::ZERO;
+                    }
+                    one_in_n = one_in_n.saturating_add(1);
+
+                    let mut acc: Self = Self::ONE;
+
+                    #[expect(clippy::arithmetic_side_effects, reason = "not with `malachite`")]
+                    while (prng.rand() % one_in_n) != 0 {
+                        acc <<= 1_u8;
+                        acc |= Self::from((prng.rand() & 1) != 0);
+
+                        one_in_n = one_in_n.saturating_add(1);
+                    }
+                    acc
+                },
+                shrink: |u| -> Box<dyn Iterator<Item = Self>> {
+                    // Copied with small (unfortunately incompatible)
+                    // modifications from `shrink_int` above.
+
+                    Box::new((0_usize..).map_while(move |shr| {
+                        #[expect(clippy::arithmetic_side_effects, reason = "not with `malachite`")]
+                        let subtrahend = &u >> shr;
+                        #[allow(
+                            clippy::allow_attributes,
+                            clippy::default_numeric_fallback,
+                            reason = "type varies"
+                        )]
+                        #[expect(
+                            clippy::arithmetic_side_effects,
+                            reason = "`u >> _` is always <= `u`"
+                        )]
+                        (subtrahend != 0).then(|| &u - subtrahend)
+                    }))
+                },
+            })
+        }
+
+        #[inline]
+        fn visit_deep<V: Construct>(&self) -> impl Iterator<Item = V> {
+            visit_self(self)
+        }
+
+        #[inline]
+        fn visit_shallow<V: Construct>(&self) -> impl Iterator<Item = &V> {
+            visit_self_opt(self).into_iter()
+        }
+    }
+}
+
 use {
     crate::{
         construct::{Construct, Literal, TypeFormer, visit_self, visit_self_opt},
@@ -23,7 +122,7 @@ macro_rules! int_corners {
 macro_rules! arbitrary_unsigned {
     // TODO: iterate over a `u64` as 64 booleans
     // instead of recomputing each
-    ($u:ty, $bits:expr, $prng:ident) => {{
+    ($u:ty, $prng:ident) => {{
         // Larger bit widths should be allowed to generate
         // huge values, whereas all integers should
         // generate relatively small numbers relatively often,
@@ -60,8 +159,8 @@ macro_rules! arbitrary_unsigned {
 /// Generate an arbitrary value for a
 /// signed integer of fixed but unspecified width.
 macro_rules! arbitrary_signed {
-    ($u:ty, $bits:expr, $prng:ident) => {{
-        let unsigned = arbitrary_unsigned!($u, $bits, $prng);
+    ($u:ty, $prng:ident) => {{
+        let unsigned = arbitrary_unsigned!($u, $prng);
         if ($prng.rand() & 1) == 0 {
             unsigned.cast_signed()
         } else {
@@ -147,7 +246,7 @@ impl Construct for u8 {
                 .into_iter()
                 .map(i8::cast_unsigned)
                 .collect(),
-            generate: |prng| arbitrary_unsigned!(Self, Self::BITS, prng),
+            generate: |prng| arbitrary_unsigned!(Self, prng),
             shrink: shrink_int!(),
         })
     }
@@ -183,7 +282,7 @@ impl Construct for u16 {
                 .into_iter()
                 .map(i16::cast_unsigned)
                 .collect(),
-            generate: |prng| arbitrary_unsigned!(Self, Self::BITS, prng),
+            generate: |prng| arbitrary_unsigned!(Self, prng),
             shrink: shrink_int!(),
         })
     }
@@ -219,7 +318,7 @@ impl Construct for u32 {
                 .into_iter()
                 .map(i32::cast_unsigned)
                 .collect(),
-            generate: |prng| arbitrary_unsigned!(Self, Self::BITS, prng),
+            generate: |prng| arbitrary_unsigned!(Self, prng),
             shrink: shrink_int!(),
         })
     }
@@ -255,7 +354,7 @@ impl Construct for u64 {
                 .into_iter()
                 .map(i64::cast_unsigned)
                 .collect(),
-            generate: |prng| arbitrary_unsigned!(Self, Self::BITS, prng),
+            generate: |prng| arbitrary_unsigned!(Self, prng),
             shrink: shrink_int!(),
         })
     }
@@ -291,7 +390,7 @@ impl Construct for u128 {
                 .into_iter()
                 .map(i128::cast_unsigned)
                 .collect(),
-            generate: |prng| arbitrary_unsigned!(Self, Self::BITS, prng),
+            generate: |prng| arbitrary_unsigned!(Self, prng),
             shrink: shrink_int!(),
         })
     }
@@ -327,7 +426,7 @@ impl Construct for usize {
                 .into_iter()
                 .map(isize::cast_unsigned)
                 .collect(),
-            generate: |prng| arbitrary_unsigned!(Self, Self::BITS, prng),
+            generate: |prng| arbitrary_unsigned!(Self, prng),
             shrink: shrink_int!(),
         })
     }
@@ -360,7 +459,7 @@ impl Construct for i8 {
     fn type_former() -> TypeFormer<Self> {
         TypeFormer::Literal(Literal {
             corners: int_corners!(i8).into_iter().collect(),
-            generate: |prng| arbitrary_signed!(u8, Self::BITS, prng),
+            generate: |prng| arbitrary_signed!(u8, prng),
             shrink: shrink_int!(),
         })
     }
@@ -393,7 +492,7 @@ impl Construct for i16 {
     fn type_former() -> TypeFormer<Self> {
         TypeFormer::Literal(Literal {
             corners: int_corners!(i16).into_iter().collect(),
-            generate: |prng| arbitrary_signed!(u16, Self::BITS, prng),
+            generate: |prng| arbitrary_signed!(u16, prng),
             shrink: shrink_int!(),
         })
     }
@@ -426,7 +525,7 @@ impl Construct for i32 {
     fn type_former() -> TypeFormer<Self> {
         TypeFormer::Literal(Literal {
             corners: int_corners!(i32).into_iter().collect(),
-            generate: |prng| arbitrary_signed!(u32, Self::BITS, prng),
+            generate: |prng| arbitrary_signed!(u32, prng),
             shrink: shrink_int!(),
         })
     }
@@ -459,7 +558,7 @@ impl Construct for i64 {
     fn type_former() -> TypeFormer<Self> {
         TypeFormer::Literal(Literal {
             corners: int_corners!(i64).into_iter().collect(),
-            generate: |prng| arbitrary_signed!(u64, Self::BITS, prng),
+            generate: |prng| arbitrary_signed!(u64, prng),
             shrink: shrink_int!(),
         })
     }
@@ -492,7 +591,7 @@ impl Construct for i128 {
     fn type_former() -> TypeFormer<Self> {
         TypeFormer::Literal(Literal {
             corners: int_corners!(i128).into_iter().collect(),
-            generate: |prng| arbitrary_signed!(u128, Self::BITS, prng),
+            generate: |prng| arbitrary_signed!(u128, prng),
             shrink: shrink_int!(),
         })
     }
@@ -525,7 +624,7 @@ impl Construct for isize {
     fn type_former() -> TypeFormer<Self> {
         TypeFormer::Literal(Literal {
             corners: int_corners!(isize).into_iter().collect(),
-            generate: |prng| arbitrary_signed!(usize, Self::BITS, prng),
+            generate: |prng| arbitrary_signed!(usize, prng),
             shrink: shrink_int!(),
         })
     }
