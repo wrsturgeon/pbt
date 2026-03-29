6fbd2f003c9078ac169cbd9029804e0d557da715
Will Sturgeon <willstrgn@gmail.com>
2026-03-29 15:38:52 -0700

    chore: better test for `pbt-tests` crate

--- pbt-tests/src/lib.rs
+++ pbt-tests/src/lib.rs
@@ -11,85 +11,32 @@ pub enum Doggo {
     },
 }
 
+impl Doggo {
+    #[inline]
+    #[must_use]
+    pub fn n_pals(&self) -> usize {
+        match *self {
+            Self::Woofer => 0, // :(
+            Self::Subwoofer { ref pals, .. } => pals.len(),
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
-    use {
-        super::*,
-        pbt::{SEED, WyRand, construct::arbitrary, size::Size},
-        pretty_assertions::assert_eq,
-    };
+    use {super::*, pbt::search, pretty_assertions::assert_eq};
 
     #[test]
-    fn deterministic_generation() {
-        let mut prng = WyRand::new(u64::from(SEED));
+    fn search_and_minimize() {
+        let popular_doggo: Option<Doggo> =
+            search::witness(1_000, |doggo: &Doggo| doggo.n_pals() >= 3);
         assert_eq!(
-            Size::expanding()
-                .take(10)
-                .filter_map(|size| arbitrary(&mut prng, size))
-                .collect::<Vec<Doggo>>(),
-            vec![
-                Doggo::Woofer,
-                Doggo::Woofer,
-                Doggo::Subwoofer {
-                    many_wow: 4,
-                    such_amaze: 1,
-                    pals: vec![],
-                },
-                Doggo::Subwoofer {
-                    many_wow: 358,
-                    such_amaze: 79,
-                    pals: vec![Doggo::Woofer],
-                },
-                Doggo::Subwoofer {
-                    many_wow: 0,
-                    such_amaze: 4,
-                    pals: vec![],
-                },
-                Doggo::Woofer,
-                Doggo::Subwoofer {
-                    many_wow: 3,
-                    such_amaze: 13823,
-                    pals: vec![Doggo::Subwoofer {
-                        many_wow: 0,
-                        such_amaze: 0,
-                        pals: vec![Doggo::Subwoofer {
-                            many_wow: 3,
-                            such_amaze: 15952,
-                            pals: vec![],
-                        }],
-                    }],
-                },
-                Doggo::Subwoofer {
-                    many_wow: 2116,
-                    such_amaze: 1,
-                    pals: vec![
-                        Doggo::Woofer,
-                        Doggo::Subwoofer {
-                            many_wow: 12083733,
-                            such_amaze: 0,
-                            pals: vec![],
-                        },
-                    ],
-                },
-                Doggo::Subwoofer {
-                    many_wow: 12,
-                    such_amaze: 5,
-                    pals: vec![Doggo::Subwoofer {
-                        many_wow: 18446744073709551615,
-                        such_amaze: 12,
-                        pals: vec![Doggo::Woofer, Doggo::Woofer],
-                    }],
-                },
-                Doggo::Subwoofer {
-                    many_wow: 6,
-                    such_amaze: 0,
-                    pals: vec![Doggo::Subwoofer {
-                        many_wow: 10,
-                        such_amaze: 945336233,
-                        pals: vec![],
-                    }],
-                },
-            ],
+            popular_doggo,
+            Some(Doggo::Subwoofer {
+                many_wow: 0,
+                such_amaze: 0,
+                pals: vec![Doggo::Woofer, Doggo::Woofer, Doggo::Woofer]
+            }),
         );
     }
 }
