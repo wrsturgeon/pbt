a19bb43d8a8f59988dce4dead649cb929ed6157e
Will Sturgeon <willstrgn@gmail.com>
2026-04-03 19:35:16 -0700

    chore: impl for `CString`

--- pbt/src/impls/strings.rs
+++ pbt/src/impls/strings.rs
@@ -12,7 +12,7 @@ use {
         size::Size,
     },
     core::{any::type_name, iter, num::NonZero},
-    std::{collections::BTreeSet, vec},
+    std::{collections::BTreeSet, ffi::CString, vec},
 };
 
 impl Construct for char {
@@ -143,5 +143,67 @@ impl Construct for String {
     fn visit_shallow<V: Construct>(&self) -> impl Iterator<Item = &V> {
         #[expect(clippy::todo, reason = "TODO")]
         visit_self_or(self, || -> vec::IntoIter<_> { todo!("lifetime issues") })
+    }
+}
+
+impl Construct for CString {
+    #[inline]
+    fn arbitrary_fields_for_ctor(
+        ctor_idx: NonZero<usize>,
+        prng: &mut wyrand::WyRand,
+        size: Size,
+    ) -> TermsOfVariousTypes {
+        let mut sizes = size.partition::<Self>(ctor_idx, prng);
+        let mut fields = TermsOfVariousTypes::new();
+        let () = fields.push(sizes.arbitrary::<Vec<u8>>(prng));
+        fields
+    }
+
+    #[inline]
+    fn register_all_immediate_dependencies(visited: &BTreeSet<Type>) {
+        let () = register::<Vec<u8>>(visited.clone());
+    }
+
+    #[inline]
+    fn type_former() -> TypeFormer<Self> {
+        TypeFormer::Algebraic(Algebraic {
+            introduction_rules: vec![IntroductionRule {
+                call: CtorFn::new(|terms| {
+                    let mut bytes: Vec<u8> = terms.must_pop();
+                    if bytes.contains(&0) {
+                        let () = bytes.retain(|&b| b != 0);
+                    }
+                    #[expect(clippy::expect_used, reason = "logically impossible")]
+                    CString::new(bytes).expect("internal `pbt` error: C-string error")
+                }),
+                immediate_dependencies: iter::once(type_of::<Vec<u8>>()).collect(),
+            }],
+            elimination_rule: ElimFn::new(|s| {
+                let mut fields = TermsOfVariousTypes::new();
+                let () = fields.push(s.into_bytes());
+                Decomposition {
+                    ctor_idx: const { NonZero::new(1).unwrap() },
+                    fields,
+                }
+            }),
+        })
+    }
+
+    #[inline]
+    fn visit_deep<V: Construct>(&self) -> impl Iterator<Item = V> {
+        visit_self::<V, Self>(self)
+            .chain(self.as_bytes().iter().flat_map(visit_self))
+            .chain({
+                let mut v = self.as_bytes().to_vec();
+                iter::from_fn(move || {
+                    let _: u8 = v.pop()?;
+                    visit_self_opt(&v).cloned()
+                })
+            })
+    }
+
+    #[inline]
+    fn visit_shallow<V: Construct>(&self) -> impl Iterator<Item = &V> {
+        visit_self_or(self, || self.as_bytes().iter().flat_map(u8::visit_shallow))
     }
 }
