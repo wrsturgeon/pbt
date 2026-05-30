73e8c49cd083c1293f771ba5ba5d0a2027022193
Will Sturgeon <willstrgn@gmail.com>
2026-05-30 16:54:00 -0700

    feat: `derive` macro for unit `struct`s

--- pbt-macro2/src/lib.rs
+++ pbt-macro2/src/lib.rs
@@ -15,16 +15,58 @@ pub fn derive_pbt(ts: TokenStream) -> TokenStream {
 /// If the input type is not up to the task.
 #[inline]
 pub fn try_derive_pbt(ts: TokenStream) -> syn::Result<TokenStream> {
+    struct Pattern {
+        construction: TokenStream,
+        deconstruction: TokenStream,
+        span: proc_macro2::Span,
+    }
+
     let DeriveInput {
         data: input_data,
         ident,
         ..
     } = syn::parse2(ts)?;
-    let syn::Data::Enum(enum_data) = input_data else {
-        return Err(syn::Error::new_spanned(
-            ident,
-            "`Pbt` can currently be derived only for enums",
-        ));
+    let patterns = match input_data {
+        syn::Data::Enum(enum_data) => enum_data
+            .variants
+            .iter()
+            .map(|variant| {
+                if !matches!(variant.fields, syn::Fields::Unit) {
+                    return Err(syn::Error::new_spanned(
+                        variant,
+                        "`Pbt` can currently be derived only for fieldless variants",
+                    ));
+                }
+                let variant_ident = &variant.ident;
+                Ok(Pattern {
+                    construction: quote::quote! { Self::#variant_ident },
+                    deconstruction: quote::quote! { Self::#variant_ident },
+                    span: variant.ident.span(),
+                })
+            })
+            .collect::<syn::Result<Vec<_>>>()?,
+        syn::Data::Struct(struct_data) => {
+            match struct_data.fields {
+                syn::Fields::Unit => {}
+                fields => {
+                    return Err(syn::Error::new_spanned(
+                        fields,
+                        "`Pbt` can currently be derived only for fieldless structs",
+                    ));
+                }
+            }
+            vec![Pattern {
+                construction: quote::quote! { Self },
+                deconstruction: quote::quote! { Self },
+                span: ident.span(),
+            }]
+        }
+        syn::Data::Union(_) => {
+            return Err(syn::Error::new_spanned(
+                ident,
+                "`Pbt` can currently be derived only for structs and enums",
+            ));
+        }
     };
 
     let literal_message = format!("`{ident}` is not a literal");
@@ -34,25 +76,20 @@ pub fn try_derive_pbt(ts: TokenStream) -> syn::Result<TokenStream> {
     let mut construct_arms = Vec::new();
     let mut deconstruct_arms = Vec::new();
     let mut register_pushes = Vec::new();
-    for (zero_index, variant) in enum_data.variants.iter().enumerate() {
-        let syn::Fields::Unit = variant.fields else {
-            return Err(syn::Error::new_spanned(
-                variant,
-                "`Pbt` can currently be derived only for fieldless variants",
-            ));
-        };
+    for (zero_index, pattern) in patterns.iter().enumerate() {
         let one_index = zero_index
             .checked_add(1)
-            .ok_or_else(|| syn::Error::new_spanned(variant, "too many variants"))?;
+            .ok_or_else(|| syn::Error::new_spanned(&ident, "too many patterns"))?;
         let one_index_string = one_index.to_string();
-        let construct_index = syn::LitInt::new(&one_index_string, variant.ident.span());
-        let deconstruct_index = syn::LitInt::new(&one_index_string, variant.ident.span());
-        let variant_ident = &variant.ident;
+        let construct_index = syn::LitInt::new(&one_index_string, pattern.span);
+        let deconstruct_index = syn::LitInt::new(&one_index_string, pattern.span);
+        let construction = &pattern.construction;
+        let deconstruction = &pattern.deconstruction;
         construct_arms.push(quote::quote! {
-            #construct_index => Self::#variant_ident
+            #construct_index => #construction
         });
         deconstruct_arms.push(quote::quote! {
-            Self::#variant_ident => ::pbt::reflection::Parts {
+            #deconstruction => ::pbt::reflection::Parts {
                 fields: {
                     let mut acc = ::pbt::fields::Store::new();
                     acc
@@ -174,6 +211,59 @@ impl ::pbt::Pbt for Bool {
             .push(::pbt::reflection::Variant {
                 field_types: ::pbt::multiset::Multiset::new(),
             });
+        let () = acc
+            .push(::pbt::reflection::Variant {
+                field_types: ::pbt::multiset::Multiset::new(),
+            });
+        ::pbt::reflection::Variants::Algebraic(acc)
+    }
+}
+"#,
+        );
+    }
+
+    #[test]
+    fn unit() {
+        expect_test(
+            r#"
+struct Unit;
+"#,
+            r#"
+impl ::pbt::Pbt for Unit {
+    #[inline]
+    fn construct<F>(
+        ::pbt::reflection::Parts { fields, variant_index }: ::pbt::reflection::Parts<F>,
+    ) -> Self
+    where
+        F: ::pbt::fields::Fields,
+    {
+        let algebraic_index: usize = variant_index
+            .expect("`Unit` is not a literal")
+            .get();
+        match algebraic_index {
+            1 => Self,
+            _ => panic!("can't instantiate variant #{algebraic_index} of `Unit`"),
+        }
+    }
+    #[inline]
+    fn deconstruct(self) -> ::pbt::reflection::Parts<::pbt::fields::Store> {
+        match self {
+            Self => {
+                ::pbt::reflection::Parts {
+                    fields: {
+                        let mut acc = ::pbt::fields::Store::new();
+                        acc
+                    },
+                    variant_index: Some(const { ::core::num::NonZero::new(1).unwrap() }),
+                }
+            }
+        }
+    }
+    #[inline]
+    fn register(
+        registration: &mut ::pbt::registration::Registration<'_>,
+    ) -> ::pbt::reflection::Variants<Self> {
+        let mut acc = vec![];
         let () = acc
             .push(::pbt::reflection::Variant {
                 field_types: ::pbt::multiset::Multiset::new(),
